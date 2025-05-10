73b5202f99550fba54704cdb04be12a7b33fc1df
chenmj <1425479236@qq.com>
2025-05-10 23:02:49 +0800

    增加相关性矩阵

--- modules.py
+++ modules.py
@@ -66,6 +66,52 @@ class DynamicGraphLearner(nn.Module):
         return adj_matrix
 
 
+class CorrelationGraphConstructor(nn.Module):
+    '''
+            Graph Constructor is the Multivariate Time Series Correlation Layer
+            (MTCL) in the paper.
+        '''
+
+    def __init__(self, nnodes, k, dim, device, alpha=3, static_feat=None):
+        super(CorrelationGraphConstructor, self).__init__()
+        self.nnodes = nnodes
+        if static_feat is not None:
+            xd = static_feat.shape[1]
+            self.lin1 = nn.Linear(xd, dim)
+            self.lin2 = nn.Linear(xd, dim)
+        else:
+            self.emb1 = nn.Embedding(nnodes, dim)
+            self.emb2 = nn.Embedding(nnodes, dim)
+            self.lin1 = nn.Linear(dim, dim)
+            self.lin2 = nn.Linear(dim, dim)
+
+        self.device = device
+        self.k = k
+        self.dim = dim
+        self.alpha = alpha
+        self.static_feat = static_feat
+
+    def forward(self, idx):
+        if self.static_feat is None:
+            nodevec1 = self.emb1(idx)
+            nodevec2 = self.emb2(idx)
+        else:
+            nodevec1 = self.static_feat[idx, :]
+            nodevec2 = nodevec1
+
+        nodevec1 = torch.tanh(self.alpha * self.lin1(nodevec1))
+        nodevec2 = torch.tanh(self.alpha * self.lin2(nodevec2))
+
+        a = torch.mm(nodevec1, nodevec2.transpose(1, 0)) - torch.mm(nodevec2, nodevec1.transpose(1, 0))
+        adj = F.relu(torch.tanh(self.alpha * a))
+        mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+        mask.fill_(float('0'))
+        s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)  # rand for numerical stability
+        mask.scatter_(1, t1, s1.fill_(1))
+        adj = adj * mask
+        return adj
+
+
 class FeatureAttentionLayer(nn.Module):
     """Single Graph Feature/Spatial Attention Layer
     :param n_features: Number of input features/nodes
@@ -276,13 +322,12 @@ class PositionalEncoding(nn.Module):
         pe = pe.unsqueeze(0)  # shape: [1, max_len, d_model]
         self.register_buffer('pe', pe)
 
-
     def forward(self, x):
-            """
-            x: Tensor, shape [batch_size, seq_len, d_model]
-            """
-            x = x + self.pe[:, :x.size(1), :]
-            return x
+        """
+        x: Tensor, shape [batch_size, seq_len, d_model]
+        """
+        x = x + self.pe[:, :x.size(1), :]
+        return x
 
 
 class GRULayer(nn.Module):
